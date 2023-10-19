74eb4d002685181ec9e142c7b89f6717c83c7840
Rodrigo Lorandi <rodrigo.lorandi@southsystem.com.br>
2023-10-19 17:38:58 -0300

    cria PersonTypeEnum

---
+++ src/main/java/com/DesafioPicPay/enums/EnumDescription.java
@@ -0,0 +1,7 @@
+package com.DesafioPicPay.enums;
+
+public interface EnumDescription {
+    String getDescription();
+
+    String name();
+}
---
+++ src/main/java/com/DesafioPicPay/enums/PersonTypeEnum.java
@@ -0,0 +1,16 @@
+package com.DesafioPicPay.enums;
+
+import com.DesafioPicPay.enums.serializer.EnumSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@JsonSerialize(using = EnumSerializer.class)
+@AllArgsConstructor
+@Getter
+public enum PersonTypeEnum {
+    PHYSICAL_PERSON("Pessoa física"),
+    JURIDIC_PERSON("Pessoa jurídica");
+
+    private final String description;
+}
