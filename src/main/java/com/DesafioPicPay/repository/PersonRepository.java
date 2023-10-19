7602b989099396cca46d75d3cf4c5ec876d9c5b8
Rodrigo Lorandi <rodrigo.lorandi@southsystem.com.br>
2023-10-19 19:11:48 -0300

    adiciona notação @Repository em PersonRepository

--- src/main/java/com/DesafioPicPay/repository/PersonRepository.java
+++ src/main/java/com/DesafioPicPay/repository/PersonRepository.java
@@ -2,9 +2,11 @@ package com.DesafioPicPay.repository;
 
 import com.DesafioPicPay.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     Optional<Person> findById(Long id);
