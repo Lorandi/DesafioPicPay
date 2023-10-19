1ab02b821a97e49b3512dd54e81693d9fba524b1
Rodrigo Lorandi <rodrigo.lorandi@southsystem.com.br>
2023-10-19 17:53:11 -0300

    cria Transfer entidade de transferência

---
+++ src/main/java/com/DesafioPicPay/entity/Transfer.java
@@ -0,0 +1,32 @@
+package com.DesafioPicPay.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity(name = "transfer")
+@Table(name = "transfer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transfer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long senderId;
+
+    private Long receiverId;
+
+    private BigDecimal value;
+
+    private LocalDateTime date;
+
+}
